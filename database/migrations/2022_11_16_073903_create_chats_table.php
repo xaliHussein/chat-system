d8c2de4f02e779b87860da904278676ea14ffa3b
Ali Hussein <xali63348@gmail.com>
2022-11-16 13:29:35 +0300

    first commit

---
+++ database/migrations/2022_11_16_073903_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->text('message')->nullable();
+            $table->string('image')->nullable();
+            $table->bigInteger('from_user')->unsigned();
+            $table->bigInteger('to_user')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+};
